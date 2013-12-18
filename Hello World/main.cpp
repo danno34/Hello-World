bd9d28d6782d157558d6d6fb6ef37b450279fe41
danno34 <diannotti12@gmail.com>
2013-12-18 15:52:58 -0500

    Initial Commit

---
+++ Hello World/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Dan on 12/18/13.
+//  Copyright (c) 2013 Cyber 5-0. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
